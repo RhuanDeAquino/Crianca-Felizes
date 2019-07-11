c2621452c242ac693f7aa2a45a1c2a32314d5059
Vai-Na-Web-05 <vainaweb@gmail.com>
2019-07-11 15:11:37 -0300

    Acabou....eu acho

--- assets/css/main.js
+++ assets/css/main.js
@@ -5,18 +5,25 @@ let eventclick = () => {
 	let compara = click % 2;
 	if (compara == 0 ){
 		menuLista.style.display = 'block'
+		menuLista.style.display = 'flex'
 		click++
 		imgLogo.style.display = 'none'
 		nav.style.width = '100%'
+		nav.style.height = '55%'
+		menuHamburguer.style.height = '50%'
 	}
 	else {
 		menuLista.style.display = 'none'
 		click++
 		imgLogo.style.display = 'block'
+		imgLogo.style.display = 'flex'
+		nav.style.width = '32%'
+		//menuHamburguer.style.height = '100%'
 	}
 	console.log(compara)
 
 }
+
 
 
 
